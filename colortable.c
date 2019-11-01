e6114af83cd7d6f6f25e163c7a4a45cbfeee1fb2
ChoiJongIn <ekejaqutmqk@gmail.com>
2019-11-01 19:56:37 +0900

    printf(033[글자색

---
+++ colortable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	for( int i = 20 ; i < 60 ; ++i)
+	{
+		for( int j = 20 ; j < 60 ; ++j)
+		{
+			printf("# \033[%d;%dm", i, j);
+		}
+		printf("\n");
+	}
+
+	printf("# \033[%d;%dm",37,40);
+
+	return 0;
+}
