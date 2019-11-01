ca772b94eea45895ec99e64c65520ad5c67edc99
ChoiJongIn <ekejaqutmqk@gmail.com>
2019-11-01 20:19:45 +0900

    printf(\033[글자색;배경색m) 설정의 유효범위를 찾기위해 20~60까지 돌려봤다

--- colortable.c
+++ colortable.c
@@ -2,16 +2,24 @@
 
 int main()
 {
+	
+	printf("   ");
+	for( int i = 20 ; i < 60 ; ++i)
+		printf("%d ",i);
+	printf("\n");	
 	for( int i = 20 ; i < 60 ; ++i)
 	{
+		printf("%d",i);
 		for( int j = 20 ; j < 60 ; ++j)
 		{
-			printf("#\033[%d;%dm", i, j);
+			printf(" # \033[%d;%dm", i, j);
 		}
+		
+		printf("# \033[%d;%dm",0,0);
 		printf("\n");
 	}
 
-	printf("# \033[%d;%dm",37,40);
 
+	printf("# \033[%d;%dm",0,0);
 	return 0;
 }
