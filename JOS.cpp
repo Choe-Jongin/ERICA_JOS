f725d41312a4ee0b1d4f6a00733842b83104cde2
ChoiJongIn <ekejaqutmqk@gmail.com>
2019-11-02 01:00:43 +0900

    된다!

--- JOS.cpp
+++ JOS.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include <locale.h>
-#include <stddef.h>
-
-#define SIZEX 40
-#define SIZEY 11
+#include <string.h>
+#include <cstring>
+#define SIZEX 160
+#define SIZEY 45
 
 using namespace std;
 
-wchar_t  g_backBuffer[SIZEY+1][SIZEX+1];
+char  g_backBuffer[SIZEY+1][SIZEX+1][4];
 
-void Draw(int _x, int _y, wchar_t _c)
+void Draw(int _x, int _y, char * _c)
 {
-	g_backBuffer[_y][_x] = _c;
+	strcpy(g_backBuffer[_y][_x],_c);
+	g_backBuffer[_y][_x][3] = '\0';
 }
 
 
@@ -25,21 +25,16 @@ int main()
 
 	//System Init
 	system("clear");
-	setlocale(LC_ALL,"");
 	
 	for( int y = 0 ; y < SIZEY ; ++y )
 	{
-	
 		for( int x = 0 ; x < SIZEX ; ++x )
 		{
-			g_backBuffer[y][x] = L' ';
-
+			Draw(x,y," ");
 		}
 
-		g_backBuffer[y][SIZEX] = L'\n';
-
+		Draw(SIZEX,y,"\n");
 	}
-
 
 	//Main Loop
 	while( true )
@@ -50,37 +45,29 @@ int main()
 		//do something in this frame
 		for( int i = 0 ; i < SIZEX ; ++i)
 		{
-			Draw(i,0,L'─');
-			Draw(i,SIZEY-1,L'─');
+			Draw(i,0,"─");
+			Draw(i,SIZEY-1,"─");
 		}
 
 		for( int i = 0 ; i < SIZEY ; ++i)
 		{
-			Draw(0,i,L'│');
-			Draw(SIZEX-1,i,L'─');
+			Draw(0,i,"│");
+			Draw(SIZEX-1,i,"│");
 		}
-		Draw(0,0,L'┌');
-		Draw(SIZEX-1,0,L'┐');
-		Draw(0,SIZEY-1,L'└');
-		Draw(SIZEX-1,SIZEY-1,L'┘');
-
+		Draw(0,0,"┌");
+		Draw(SIZEX-1,0,"┐");
+		Draw(0,SIZEY-1,"└");
+		Draw(SIZEX-1,SIZEY-1,"┘");
+		Draw(0,SIZEY,"\0");
 		//render
-//		wprintf(L"%s", g_backBuffer[0]);
 		for( int i = 0 ; i < SIZEY ; ++i)
-			for(int j = 0 ; j < SIZEX ; ++j)
-	//			wprintf(L"%c",g_backBuffer[i][j]);
-				;
+			for(int j = 0 ; j <= SIZEX ; ++j)
+				printf("%s",g_backBuffer[i][j]);
 
-
-//		wprintf(L"%c",L'─');
 		break;
 	}
-
 	
 	cout<<"JOS IS END"<<endl;
-	cout<<"JOS IS END"<<endl;
 
-//	Gotoxy(5,10);
-//	cout << " Start the JOS! " << endl;
 	return 0;
 }
