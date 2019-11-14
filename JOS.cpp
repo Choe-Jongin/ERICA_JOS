8e848e0f1abbc4f924d63c379485ae889a94d378
ChoiJongIn <ekejaqutmqk@gmail.com>
2019-11-14 13:50:30 +0900

    kbhit수정

--- JOS.cpp
+++ JOS.cpp
@@ -146,4 +146,16 @@ int Kbhit(void)
   tcgetattr(STDIN_FILENO, &oldt);
   newt = oldt;
   newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+  ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+  return 0;
 }
